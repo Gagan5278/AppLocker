2b21798d6231151a9d7f12afbff98f4c46cd65ff
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2024-01-11 20:05:58 +0100

    Initial Commit

---
+++ AppLockerView/AppLockerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLockerViewApp.swift
+//  AppLockerView
+//
+//  Created by Gagan Vishal  on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct AppLockerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
